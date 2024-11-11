d76f864cb6fb5ba17a903ff4610386da23d9e14d
MrAMB312 <a.blomarz@gmail.com>
2024-11-10 21:53:05 -0600

    complete phase 1 sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const id = object.id;
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.textContent = `New ID: ${id}`;
+            body.appendChild(p);
+        })
+        .catch(function (error) {
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.textContent = `Error: ${error.message}`;
+            body.appendChild(p);
+        });
+}
